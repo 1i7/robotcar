9909df697f12505865c1c4ba26d8d8d9354b6a20
1i7 <ii1i7@yandex.ru>
2016-06-14 16:22:00 +0300

    Экспортировать колеса с разными значениями print_error в разные каталоги

--- model1/колеса/export-wheels.sh
+++ model1/колеса/export-wheels.sh
@@ -7,20 +7,20 @@ openscad -D "left=true; axis_type=2; print_error=0" -o export/wheel-d3mm-ds-left
 openscad -D "left=false; axis_type=2; print_error=0" -o export/wheel-d3mm-ds-right.stl колесо-4export.scad
 
 # print_error=0.1
-openscad -D "left=true; axis_type=1; print_error=0.1" -o export/wheel-d2mm-left-pe0.1.stl колесо-4export.scad
-openscad -D "left=false; axis_type=1; print_error=0.1" -o export/wheel-d2mm-right-pe0.1.stl колесо-4export.scad
-openscad -D "left=true; axis_type=2; print_error=0.1" -o export/wheel-d3mm-ds-left-pe0.1.stl колесо-4export.scad
-openscad -D "left=false; axis_type=2; print_error=0.1" -o export/wheel-d3mm-ds-right-pe0.1.stl колесо-4export.scad
+openscad -D "left=true; axis_type=1; print_error=0.1" -o export/pe0.1/wheel-d2mm-left-pe0.1.stl колесо-4export.scad
+openscad -D "left=false; axis_type=1; print_error=0.1" -o export/pe0.1/wheel-d2mm-right-pe0.1.stl колесо-4export.scad
+openscad -D "left=true; axis_type=2; print_error=0.1" -o export/pe0.1/wheel-d3mm-ds-left-pe0.1.stl колесо-4export.scad
+openscad -D "left=false; axis_type=2; print_error=0.1" -o export/pe0.1/wheel-d3mm-ds-right-pe0.1.stl колесо-4export.scad
 
 # print_error=0.2
-openscad -D "left=true; axis_type=1; print_error=0.2" -o export/wheel-d2mm-left-pe0.2.stl колесо-4export.scad
-openscad -D "left=false; axis_type=1; print_error=0.2" -o export/wheel-d2mm-right-pe0.2.stl колесо-4export.scad
-openscad -D "left=true; axis_type=2; print_error=0.2" -o export/wheel-d3mm-ds-left-pe0.2.stl колесо-4export.scad
-openscad -D "left=false; axis_type=2; print_error=0.2" -o export/wheel-d3mm-ds-right-pe0.2.stl колесо-4export.scad
+openscad -D "left=true; axis_type=1; print_error=0.2" -o export/pe0.2/wheel-d2mm-left-pe0.2.stl колесо-4export.scad
+openscad -D "left=false; axis_type=1; print_error=0.2" -o export/pe0.2/wheel-d2mm-right-pe0.2.stl колесо-4export.scad
+openscad -D "left=true; axis_type=2; print_error=0.2" -o export/pe0.2/wheel-d3mm-ds-left-pe0.2.stl колесо-4export.scad
+openscad -D "left=false; axis_type=2; print_error=0.2" -o export/pe0.2/wheel-d3mm-ds-right-pe0.2.stl колесо-4export.scad
 
 # print_error=0.2
-openscad -D "left=true; axis_type=1; print_error=0.3" -o export/wheel-d2mm-left-pe0.3.stl колесо-4export.scad
-openscad -D "left=false; axis_type=1; print_error=0.3" -o export/wheel-d2mm-right-pe0.3.stl колесо-4export.scad
-openscad -D "left=true; axis_type=2; print_error=0.3" -o export/wheel-d3mm-ds-left-pe0.3.stl колесо-4export.scad
-openscad -D "left=false; axis_type=2; print_error=0.3" -o export/wheel-d3mm-ds-right-pe0.3.stl колесо-4export.scad
+openscad -D "left=true; axis_type=1; print_error=0.3" -o export/pe0.3/wheel-d2mm-left-pe0.3.stl колесо-4export.scad
+openscad -D "left=false; axis_type=1; print_error=0.3" -o export/pe0.3/wheel-d2mm-right-pe0.3.stl колесо-4export.scad
+openscad -D "left=true; axis_type=2; print_error=0.3" -o export/pe0.3/wheel-d3mm-ds-left-pe0.3.stl колесо-4export.scad
+openscad -D "left=false; axis_type=2; print_error=0.3" -o export/pe0.3/wheel-d3mm-ds-right-pe0.3.stl колесо-4export.scad
 
